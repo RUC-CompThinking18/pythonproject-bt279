761f0e196236c7c8483393818b6bfbcc5fcc50d8
bt279 <42982070+bt279@users.noreply.github.com>
2018-10-31 16:58:30 -0400

    Wrote my first function for the python project
    wrote a function to mix 2 strings together

---
+++ title_mixer.py
@@ -0,0 +1,9 @@
+import random
+
+def mixer(game_name, misconduct):
+    game_array = game_name.split() ##splits the game name into an array
+    if len(game_array) == 1: ##if the game title only has 1 word in it, it puts the misconduct at the end of it
+        game_array.append(misconduct)
+    placement = random.randint(0, len(game_array)) ##picks a random word in the game title for the misconduct to replace
+    game_array[placement] = misconduct
+    return game_array
