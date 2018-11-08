78c45cec1d4b7f09b8aa1ce3ec22f51d8e7d97c0
bt279 <42982070+bt279@users.noreply.github.com>
2018-11-08 15:44:26 -0500

    Added a comment
    Added the comment for the whole function, as I only had individual line comments.

--- title_mixer.py
+++ title_mixer.py
@@ -1,6 +1,8 @@
 import random
 
 def mixer(game_name, misconduct):
+    """This is an internal function that will be used to actually mix up the game titles and the misconducts that the bigger function
+    for this assignment will use."""
     game_array = game_name.split() ##splits the game name into an array
     if len(game_array) == 1: ##if the game title only has 1 word in it, it puts the misconduct at the end of it
         game_array.append(misconduct)
