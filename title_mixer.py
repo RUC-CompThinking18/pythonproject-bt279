e8b9abba6ea0c86f3e9fc6fcd4667ac0250bb253
bt279 <42982070+bt279@users.noreply.github.com>
2018-11-08 16:33:00 -0500

    Added access to API and started on the bot function
    Basically what I wrote in the title. Not much else.

--- title_mixer.py
+++ title_mixer.py
@@ -1,4 +1,8 @@
 import random
+from igdb_api_python.igdb import igdb ##getting access to the igdb video game database
+
+igdb = igdb("dc4da56a02c6038e2ea17b7b1ff4ce7d") ##key for using api
+
 
 def mixer(game_name, misconduct):
     """This is an internal function that will be used to actually mix up the game titles and the misconducts that the bigger function
@@ -10,3 +14,12 @@ def mixer(game_name, misconduct):
     game_array[placement] = misconduct
     mixed_game_name = game_array.join(" ")
     return mixed_game_name
+
+def bot():
+    """The actual "twitter bot" that will use the mixer function above after grabbing a random game from the video game database
+    and a random misconduct from a list and putting them into the mixer function. """
+    random_game = random.randint(1, 109335) ##grabs a random int
+    result = igdb.games(random_game) ##uses the random in as an id for a game
+    for game in result.body:
+        chosen_game = game["name"] ##stores the game name as it's own variable
+    
